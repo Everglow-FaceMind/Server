ebe3f97a9db593947ae48fbfa1275c314516c730
jorippppong <duelee75@gmail.com>
2024-04-05 15:53:25 +0900

    first commit

---
+++ src/main/java/com/facemind/FaceMindApplication.java
@@ -0,0 +1,13 @@
+package com.facemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceMindApplication.class, args);
+    }
+
+}
