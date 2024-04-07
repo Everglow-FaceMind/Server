3bb8a31e4c12a2f236dc70848823a4a9564afb7d
jorippppong <duelee75@gmail.com>
2024-04-07 21:25:11 +0900

    [FEAT #1] 회원가입, 로그인 관련 entity

---
+++ src/main/java/com/facemind/app/domain/Member.java
@@ -0,0 +1,21 @@
+package com.facemind.app.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/facemind/app/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.facemind.app.repository;
+
+import com.facemind.app.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
