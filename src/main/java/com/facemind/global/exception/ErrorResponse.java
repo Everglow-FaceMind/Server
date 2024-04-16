62e2ca9fc9a83c2bf206231b633da56f147b62d8
jorippppong <duelee75@gmail.com>
2024-04-16 16:12:08 +0900

    [FEAT #4] 에러 응답 통일

---
+++ src/main/java/com/facemind/global/exception/ErrorResponse.java
@@ -0,0 +1,57 @@
+package com.facemind.global.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class ErrorResponse {
+    private final boolean isSuccess = false;
+    private final HttpStatus httpStatus;
+    private final int code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final List<ValidationError> errors;
+
+    public static ErrorResponse of(HttpStatus httpStatus, int code, String message){
+        return ErrorResponse.builder()
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .build();
+    }
+
+    public static ErrorResponse of(HttpStatus httpStatus, int code, String message, BindingResult bindingResult){
+        return ErrorResponse.builder()
+                .httpStatus(httpStatus)
+                .code(code)
+                .message(message)
+                .errors(ValidationError.of(bindingResult))
+                .build();
+    }
+
+    @Getter
+    public static class ValidationError{
+        private final String field;
+        private final String message;
+
+        private ValidationError(FieldError fieldError){
+            this.field = fieldError.getField();
+            this.message = fieldError.getDefaultMessage();
+        }
+
+        private static List<ValidationError> of(final BindingResult bindingResult){
+            return bindingResult.getFieldErrors().stream()
+                    .map(ValidationError::new)
+                    .toList();
+        }
+    }
+}
